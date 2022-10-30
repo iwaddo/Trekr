be5eccf7dc0ed44192b6caf6c2411e3b838f8b00
Ian Waddington <iwaddo@icloud.com>
2022-10-30 19:31:28 +0000

    Initial Commit

---
+++ Trekr/TrekrApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekrApp.swift
+//  Trekr
+//
+//  Created by Ian Waddington on 30/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct TrekrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
